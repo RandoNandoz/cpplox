225247a1a84cd2da4bc85008be8e307f23d900dc
Randy Zhu <randy@randyzhu.com>
2025-01-17 09:47:14 -0800

    Implement runfile

--- src/lox/Lox.cpp
+++ src/lox/Lox.cpp
@@ -1,14 +1,13 @@
 #include "Lox.hpp"
-#include <iostream>
-#include <fstream>
-#include <sstream>
+#include "filereader/FileReader.hpp"
 
 void Lox::runFile(const std::string& path) {
-
+    FileReader reader(path);
+    this->run(reader.readAsString());
 }
 
 void Lox::runREPL() {
-
+    
 }
 
 void Lox::run(const std::string& source) {
